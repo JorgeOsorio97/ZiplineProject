7f9e5e0d820045cbf10d5c7362443db07907f1e4
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2018-03-11 22:50:01 -0600

    avances en MA zipline se queda trabajando sin avance revisar si hay ciclo perdido sze usa talib y zipline en conjunto

--- MA-zipline.py
+++ MA-zipline.py
@@ -1,19 +1,18 @@
 import pandas_datareader as web
 from zipline.api import order,record,symbol
 from zipline.algorithm import TradingAlgorithm
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime as dt
 from collections import OrderedDict
 import pytz
-from talib import EMA, MA, SAR
+from talib import MA
 
 data = OrderedDict()
 start = dt.datetime(2015,1,1)
 end = dt.datetime(2015,12,31)
 
-asset = 'AAPL'
+asset = 'AAPL'  
 
 data[asset] = web.DataReader(asset, 'google', start= start, end=end)
 
@@ -31,34 +30,15 @@ def initialize(context):
 
 def handle_data(context, data):
     long_period = 40
-    if context.count < long_period:
-        context.count+=1
-        return
-    price_history = data.history(context.asset, 'price', 40, '1d')
-    #if trailing_window.isnull().values.any():
-    #    return
-    short_MA = MA(price_history.values, 20)
-    long_MA = MA(price_history.values, long_period)
-    print(short_MA)
-    print(long_MA)
-    
-
-
-"""def handle_data(context, data):
     short_period = 20
-    long_period = 100 
+    context.count+=1
     if context.count < long_period:
         return
-    else:
-        short_MA = MA(data.history(context.asset, 'price', 20, '1d'),short_period)
-        long_MA = MA(data.history(context.asset, 'price', 100, '1d'), long_period)
-        print(long_MA)
-        print(short_MA)
-        
-    context.count += 1
-"""
-
-
+    price_history = data.history(context.asset, 'price', long_period, '1d')
+    short_MA = MA(price_history.values, 20)
+    long_MA = MA(price_history.values,40)
+    record(price = data.current(context.asset, 'price'), promedioMovilCorto = short_MA, promedioMovilLargo = long_MA) 
+    
 
 algo_obj = TradingAlgorithm(initialize = initialize, handle_data = handle_data)
 
